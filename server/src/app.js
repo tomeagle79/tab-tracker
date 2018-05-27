46357b6687c85eb9a10152ce7133927c99cbac54
Tom Eagle <tom@dxdigital.co.uk>
2018-05-27 13:27:11 +0100

    setting up app.js with includes and routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
-console.log('hello bongo')
+console.log('hello mongo')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan') // morgan logs out messages to console eg where the app is being run
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
